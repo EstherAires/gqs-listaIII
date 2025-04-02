1a108f1b52c0a01c016bc3ca59d7707ce7bfd768
EstherAires <estherlourenco777@gmail.com>
2025-04-02 01:29:18 -0300

    att1 - media aritmetica

---
+++ att1/Att1MediaAritmetica.java
@@ -0,0 +1,31 @@
+package att1;
+import java.util.Scanner;
+
+
+class Calculadora{
+    public double calculaMedia(double n1, double n2) {
+        return ( (n1 + n2) / 2);
+    }
+}
+
+public class Att1MediaAritmetica {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calculadora calculadora = new Calculadora();
+
+        double n1, n2, media;
+        
+        System.out.println("Informe o primeiro número: ");
+        n1 = sc.nextDouble();
+        
+        System.out.println("Informe o segundo número: \n");
+        n2 = sc.nextDouble();
+        
+        media = calculadora.calculaMedia(n1, n2);
+        
+        System.out.println("------------------------------");
+        System.out.println("A média aritmética é: " + media + "\n");
+        
+        sc.close();
+    }
+}
